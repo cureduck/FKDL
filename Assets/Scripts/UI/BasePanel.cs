6abe520a81a9eeda6fbe0e1f77940ee1cbca00e1
cureduck <842483592@qq.comm>
2023-03-26 15:27:30 +0800

    新UI基类测试

---
+++ Assets/Scripts/UI/BasePanel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public abstract class BasePanel<T> : MonoBehaviour where T : IUpdateable
+    {
+        public T Data
+        {
+            get => _data;
+            set => SetData(value);
+        }
+
+        private T _data;
+        
+
+        private void SetData(T d)
+        {
+            if (_data != null)
+            {
+                RemoveListener();
+            }
+            _data = d;
+
+            AddListener();
+            
+            UpdateUI();
+        }
+
+
+        private void AddListener()
+        {
+            if (_data == null)
+            {
+                return;
+            }
+            _data.OnUpdated += UpdateUI;
+            _data.OnDestroy -= UpdateUI;
+        }
+
+        private void RemoveListener()
+        {
+            _data.OnUpdated -= UpdateUI;
+            _data.OnDestroy -= RemoveListener;
+        }
+
+        protected abstract void UpdateUI();
+        
+        protected virtual void OnOpen(){}
+        protected virtual void OnClose(){}
+    }
+}
---
+++ Assets/Scripts/UI/NewGoldPanel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Game;
+using Managers;
+using TMPro;
+
+namespace UI
+{
+    public class NewGoldPanel : BasePanel<PlayerData>
+    {
+        public TMP_Text gold;
+
+        protected void Start()
+        {
+            Data = GameManager.Instance.PlayerData;
+            GameManager.Instance.GameLoaded +=
+                () => { Data = GameManager.Instance.PlayerData; };
+        }
+
+        protected override void UpdateUI()
+        {
+            if (Data != null)
+            {
+                gold.text = Data.Gold.ToString();
+            }
+        }
+    }
+}
